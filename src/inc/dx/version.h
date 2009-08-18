ee76debe855b3cad408f605f35d135e0e9d528ec
Dan Gaur <Dan Gaur@localhost>
2009-08-17 23:00:49 -0700

    Automatic update for version 0.0.1a

--- src/inc/dx/version.h
+++ src/inc/dx/version.h
@@ -13,8 +13,8 @@
 //
 // Current version number
 //
-#define DX_VERSION         "0.0.1"
-#define DX_RELEASE_DATE    "2009-08-16"
+#define DX_VERSION         "0.0.1a"
+#define DX_RELEASE_DATE    "2009-08-17"
 
 
 
