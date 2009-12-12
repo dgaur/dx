09f363593cb08a2416a2b2814da26d645f6a22af
Dan Gaur <dgaur@kenai.com>
2009-12-11 20:12:46 -0800

    Added ptrdiff_t

--- src/inc/stddef.h
+++ src/inc/stddef.h
@@ -10,6 +10,10 @@
 #include "size_t.h"
 
 
+// Definition of ptrdiff_t: type of result of subtracting pointers
+typedef signed long		ptrdiff_t;
+
+
 // Different definitions of NULL for C and C++
 #if defined(__cplusplus)
 #define NULL 0
