dcea963f96f2662c929dcfb27620bad7b4931696
Dan Gaur <dgaur@kenai.com>
2009-12-16 21:07:23 -0800

    Added iso646.h

---
+++ src/inc/iso646.h
@@ -0,0 +1,25 @@
+//
+// iso646.h
+//
+// Alternative spellings
+//
+
+#ifndef _ISO646_H
+#define _ISO646_H
+
+
+#define and			&&
+#define and_eq		&=
+#define bitand		&
+#define bitor		|
+#define compl		~
+#define not			!
+#define not_eq		!=
+#define or			||
+#define or_eq		|=
+#define xor			^
+#define xor_eq		^=
+
+
+#endif
+
