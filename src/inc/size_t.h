e3220449eb7df1f5eef183ade1cd86803510f9fa
dan <dan@dangaur.com>
2011-08-13 22:08:48 -0700

    Minor infrastructure for dealing with size_t definititions.
    Would be better if the configure script would figure this out automatically.

--- src/inc/size_t.h
+++ src/inc/size_t.h
@@ -12,7 +12,17 @@
 // Use a single definition here that can be #include'd from these (or other
 // headers) as necessary
 //
-typedef unsigned long size_t;
+// Various versions + installations of gcc expect size_t to be either
+// "unsigned long" or "unsigned int".  Would be nice if configure script could
+// figure this out, but cross-tools make this difficult.
+//
+#ifdef SIZET_IS_ULONG
+	typedef unsigned long size_t;
+#elif SIZET_IS_UINT
+	typedef unsigned int  size_t;
+#else
+	#error "Definition of size_t is unknown.  See Makefile.src"
+#endif
 
 
 #endif
