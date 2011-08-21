36818a520b22f6bea1d3561dafff5779705c20d6
Dan Gaur <dan@dangaur.com>
2011-08-20 21:18:39 -0700

    Automatic update after committing version 0.0.5

--- src/inc/dx/version.h
+++ src/inc/dx/version.h
@@ -13,8 +13,8 @@
 //
 // Current version number
 //
-#define DX_VERSION         "0.0.5"
-#define DX_RELEASE_DATE    "2011-08-20"
+#define DX_VERSION         "post-0.0.5"
+#define DX_RELEASE_DATE    "unreleased"
 
 
 
