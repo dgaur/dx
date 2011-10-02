94feca4ec22104be629cbaa7957bc88c402e660c
Dan Gaur <dan@dangaur.com>
2011-10-01 22:25:39 -0700

    Add terminator to end of output string. Fixes runaway string output in previous commit.

--- src/common/libc/puts.c
+++ src/common/libc/puts.c
@@ -21,16 +21,17 @@ int
 puts(const char *string)
 	{
 	size_t		length = strlen(string);
-	size_t		length_with_newline = length + 1;
+	size_t		length_with_newline = length + 1 + 1;	// Newline + terminator
 
 	//
 	// Per the C99 spec, puts() automatically appends a newline to its output.
 	// Build a string containing the input string plus the newline
 	//
-	char buffer[ length_with_newline ];		// No terminator
+	char buffer[ length_with_newline ];
 	memcpy(buffer, string, length);
-	buffer[ length ] = '\n';
-
+	buffer[ length + 0 ] = '\n';
+	buffer[ length + 1 ] = 0;
+ 
 	return(fputs(buffer, stdout));
 	}
 
