68f954fd2179dc978ba46224cb31da71a41737e3
Dan Gaur <dan@dangaur.com>
2011-12-11 21:43:12 -0800

    Automatic update for version 0.1.0

--- src/inc/dx/version.h
+++ src/inc/dx/version.h
@@ -13,8 +13,8 @@
 //
 // Current version number
 //
-#define DX_VERSION         "post-0.0.7"
-#define DX_RELEASE_DATE    "unreleased"
+#define DX_VERSION         "0.1.0"
+#define DX_RELEASE_DATE    "2011-12-11"
 
 
 
